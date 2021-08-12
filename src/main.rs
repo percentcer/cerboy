845078cf9dcf80b51cb6c11fa9f01275a6e3f4b4
Spencer Miller <spence.miller@gmail.com>
2021-08-12 15:26:32 -0700

    adds system memory

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,42 @@ const fn reset() -> CPUState {
     }
 }
 
+fn reset_mem() -> Vec<Byte> {
+    let mut mem: Vec<Byte> = vec![0; Word::MAX as usize + 1];
+    mem[0xFF05] = 0x00; //TIMA
+    mem[0xFF06] = 0x00; //TMA
+    mem[0xFF07] = 0x00; //TAC
+    mem[0xFF10] = 0x80; //NR10
+    mem[0xFF11] = 0xBF; //NR11
+    mem[0xFF12] = 0xF3; //NR12
+    mem[0xFF14] = 0xBF; //NR14
+    mem[0xFF16] = 0x3F; //NR21
+    mem[0xFF17] = 0x00; //NR22
+    mem[0xFF19] = 0xBF; //NR24
+    mem[0xFF1A] = 0x7F; //NR30
+    mem[0xFF1B] = 0xFF; //NR31
+    mem[0xFF1C] = 0x9F; //NR32
+    mem[0xFF1E] = 0xBF; //NR33
+    mem[0xFF20] = 0xFF; //NR41
+    mem[0xFF21] = 0x00; //NR42
+    mem[0xFF22] = 0x00; //NR43
+    mem[0xFF23] = 0xBF; //NR30
+    mem[0xFF24] = 0x77; //NR50
+    mem[0xFF25] = 0xF3; //NR51
+    mem[0xFF26] = 0xF1; // NR52 (note: $F0 on Super GB)
+    mem[0xFF40] = 0x91; // LCDC
+    mem[0xFF42] = 0x00; // SCY
+    mem[0xFF43] = 0x00; // SCX
+    mem[0xFF45] = 0x00; // LYC
+    mem[0xFF47] = 0xFC; // BGP
+    mem[0xFF48] = 0xFF; // OBP0
+    mem[0xFF49] = 0xFF; // OBP1
+    mem[0xFF4A] = 0x00; // WY
+    mem[0xFF4B] = 0x00; // WX
+    mem[0xFFFF] = 0x00; // IE
+    mem
+}
+
 const fn hi(reg: Word) -> Byte { (reg >> Byte::BITS) as Byte }
 const fn lo(reg: Word) -> Byte { (reg & LOW_MASK) as Byte }
 
@@ -401,6 +437,10 @@ fn main() -> Result<(), Error> {
         Err(file) => panic!("failed to open {}", file)
     };
     file.read(&mut rom).expect("failed to read file into memory");
+
+    // memory stuff
+    // ------------
+    let mut mem: Vec<Byte> = reset_mem();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;

03eaecf15f2ba1aaee88e8fa4a40cc7bd922a6d0
Spencer Miller <spence.miller@gmail.com>
2021-08-12 15:52:30 -0700

    switch to minifb instead of pixels for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-use winit_input_helper::WinitInputHelper;
+extern crate minifb;
+use minifb::{Key, Window, WindowOptions};
 
 use log::{error};
 extern crate env_logger;
-
-use pixels::{
-    Error, SurfaceTexture, PixelsBuilder, wgpu
-};
 
 use std::io::Read;
 
@@ -44,8 +36,8 @@ use std::io::Read;
 // FF80-FFFE   High RAM (HRAM)
 // FFFF        Interrupt Enable Register
 
-const GB_SCREEN_WIDTH : u32 = 160;
-const GB_SCREEN_HEIGHT: u32 = 144;
+const GB_SCREEN_WIDTH : usize = 160;
+const GB_SCREEN_HEIGHT: usize = 144;
 const ROM_MAX: usize = 0x200000;
 
 type Byte = u8;
@@ -399,32 +391,22 @@ fn jp(cpu: CPUState, low: Byte, high: Byte) -> CPUState {
 //   reti           D9          16 ---- return and enable interrupts (IME=1)
 //   rst  n         xx          16 ---- call to 00,08,10,18,20,28,30,38
 
-fn main() -> Result<(), Error> {
+fn main() {
     env_logger::init();
 
     // window management
     // -----------------
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-    .with_title("cerboy")
-    .build(&event_loop)
-    .unwrap();
-    let min_size: winit::dpi::LogicalSize<f64> =
-    winit::dpi::PhysicalSize::new(GB_SCREEN_WIDTH, GB_SCREEN_HEIGHT)
-    .to_logical(window.scale_factor());
-    window.set_min_inner_size(Some(min_size));
-
-    let mut input = WinitInputHelper::new();
-
-    // surface
-    // -------
-    let surface_texture = SurfaceTexture::new(window.inner_size().width, window.inner_size().height, &window);
-    let mut pixels = PixelsBuilder::new(GB_SCREEN_WIDTH, GB_SCREEN_HEIGHT, surface_texture)
-    .request_adapter_options(wgpu::RequestAdapterOptions {
-        power_preference: wgpu::PowerPreference::HighPerformance,
-        compatible_surface: None,
-    })
-    .build()?;
+    let mut buffer: Vec<u32> = vec![0; GB_SCREEN_WIDTH * GB_SCREEN_HEIGHT];
+    let mut window = Window::new(
+        "cerboy",
+        GB_SCREEN_WIDTH * 4,
+        GB_SCREEN_HEIGHT * 4,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}",e)
+    });
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
     // rom stuff
     // ---------
@@ -442,34 +424,20 @@ fn main() -> Result<(), Error> {
     // ------------
     let mut mem: Vec<Byte> = reset_mem();
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        if let Event::RedrawRequested(_) = event {
-            for (i, pixel) in pixels.get_frame().chunks_exact_mut(4).enumerate() {
-                let slice = [(i % 2 * 0xFF) as u8, (i % GB_SCREEN_WIDTH as usize) as u8, 0x00, 0xFF];
-                pixel.copy_from_slice(&slice)
-            }
-            if pixels.render().map_err(|e| error!("pixels.render() has failed: {}", e))
-            .is_err() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
+    // loop
+    // ------------
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let mut c: u32 = 0;
+        for i in buffer.iter_mut() {
+            *i = (c % 2 * 0xff00ff);
+            c += 1;
         }
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => {
-                if input.update(&event) {
-                    if let Some(size) = input.window_resized() {
-                        pixels.resize_surface(size.width, size.height);
-                    }
-                    window.request_redraw();
-                }
-            },
-        }
-    });
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, GB_SCREEN_WIDTH, GB_SCREEN_HEIGHT)
+            .unwrap();
+    }
 }
 
 #[cfg(test)]

8d96375305c3634314f201bdc2acc89fe5d88a8b
Spencer Miller <spence.miller@gmail.com>
2022-11-03 10:43:03 -0700

    [dis] tidy up instruction printing

--- src/bin/dis.rs
+++ src/bin/dis.rs
@@ -36,23 +36,23 @@ fn main() {
     let mut i = 0;
     while i < rom.len() {
         let inst: Instruction = decode(rom[i]);
-        if inst.valid() {
-            if inst.prefix() {
-                i += 1; // (all cb instructions are 1 byte for the prefix and 1 byte for the opcode)
-                let cb: Instruction = decodeCB(rom[i]);
-                println!("{}", cb.mnm);
-            } else {
-                match inst.len {
-                    1 => println!("{}", inst.mnm),
-                    2 => println!("{}", inst.mnm_args(&rom[i+1..i+2])),
-                    3 => println!("{}", inst.mnm_args(&rom[i+1..i+3])),
-                    _ => panic!("(unreachable) todo: this is getting messy"),
-                }
-            }
-            i += inst.len as usize;
-        } else {
+        if !inst.valid() {
+            i += 1;
             println!("[invalid instruction]");
-            i += 1
+            continue;
         }
+        let argc = inst.len as usize;
+        if inst.prefix() {
+            i += 1; // (all cb instructions are 1 byte for the prefix and 1 byte for the opcode)
+            let cb: Instruction = decodeCB(rom[i]);
+            println!("{}", cb.mnm);
+        } else {
+            match argc {
+                1 => println!("{}", inst.mnm),
+                2..=3 => println!("{}", inst.mnm_args(&rom[i+1..i+argc])),
+                _ => panic!("(unreachable) todo: this is getting messy"),
+            }
+        }
+        i += argc;
     }
 }
